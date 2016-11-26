0966bac8ea32a762bcda4fdbf4921a3bef170b3b
Sergey Uchaev <dialoggg1994@gmail.com>
2016-11-26 16:47:33 +0700

    add stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     connect = require('gulp-connect'),
-    sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     jsmin = require('gulp-jsmin'),
     rename = require('gulp-rename'),
     cssmin = require('gulp-cssmin'),
+    stylus = require('gulp-stylus'),
     imageop = require('gulp-image-optimization');
 gulp.task('default', ['connect', 'watch']);
 
@@ -16,7 +16,7 @@ gulp.task('connect', function () {
 });
 gulp.task('watch', function () {
     gulp.watch(['index.html'], ['html']);
-    gulp.watch(['src/style/sass/*.scss'], ['sass']);
+    gulp.watch(['src/style/stylus/*.styl'], ['stylus']);
     gulp.watch(['src/style/css/*.css'], ['production-css']);
     gulp.watch(['src/js/*.js'], ['production-js']);
     gulp.watch(['src/img/*.js'], ['production-img']);
@@ -25,22 +25,22 @@ gulp.task('html', function () {
     gulp.src('index.html')
         .pipe(connect.reload());
 });
-gulp.task('sass', function () {
-    return gulp.src('src/style/sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
+gulp.task('stylus', function () {
+    return gulp.src('src/style/stylus/*.styl')
+        .pipe(stylus()) 
         .pipe(gulp.dest('src/style/css/'))
         .pipe(connect.reload());
 });
 
 gulp.task('production-js', function () {
-    return gulp.src(['src/js/'])
+    return gulp.src(['src/js/*.js'])
         .pipe(concat('all.js'))
         .pipe(jsmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('build/js/'));
 });
 gulp.task('production-css', function () {
-    gulp.src(['src/style/css/'])
+    gulp.src(['src/style/css/*.css'])
         .pipe(concat('style.css'))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
--- readme.md
+++ readme.md
@@ -1,13 +1,4 @@
-starter pack front-end
-=================
-*Gulp
- *"gulp": "^3.9.1",
-   * "gulp-concat": "^2.6.0",
-   * "gulp-connect": "^5.0.0",
-   * "gulp-cssmin": "^0.1.7",
-  *  "gulp-image-optimization": "^0.1.3",
-  *  "gulp-jsmin": "^0.1.5",
-   * "gulp-sass": "^2.3.2",
-  *  "gulp-watch": "^4.3.9",
-  *  "node-sass": "^3.10.0"
-*Sass
+# starter-pack
+
+1. Установка всех модулей npm update
+2. Сборка проекта и watch запускается командой gulp
